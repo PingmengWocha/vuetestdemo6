2b13a10d20956929f20eebd0d97eaf9b1c6de8e4
xiazhuhai <xiazhuhai@njbandou.onaliyun.com>
2019-12-10 15:40:15 +0800

    添加导航栏

---
+++ src/components/content/mainTabbar/MainTabBar.vue
@@ -0,0 +1,40 @@
+<template>
+  <TabBar>
+    <TabBarItem path="/home" activeColor="deepPink">
+      <img src="~assets/img/tabbar/home.svg" slot="item-icon">
+      <img src="~assets/img/tabbar/home_active.svg" slot="item-icon-active">
+      <div slot="item-text">首页</div>
+    </TabBarItem>
+    <TabBarItem path="/category" activeColor="deepPink">
+      <img src="~assets/img/tabbar/category.svg" slot="item-icon">
+      <img src="~assets/img/tabbar/category_active.svg" slot="item-icon-active">
+      <div slot="item-text">分类</div>
+    </TabBarItem>
+    <TabBarItem path="/cart" activeColor="deepPink">
+      <img src="~assets/img/tabbar/cart.svg" slot="item-icon">
+      <img src="~assets/img/tabbar/cart_active.svg" slot="item-icon-active">
+      <div slot="item-text">购物车</div>
+    </TabBarItem>
+    <TabBarItem path="/profile" activeColor="deepPink">
+      <img src="~assets/img/tabbar/profile.svg" slot="item-icon">
+      <img src="~assets/img/tabbar/profile_active.svg" slot="item-icon-active">
+      <div slot="item-text">我的</div>
+    </TabBarItem>
+  </TabBar>
+</template>
+
+<script>
+  import TabBar from '../../common/tabbar/TabBar'
+  import TabBarItem from '../../common/tabbar/TarBarItem'
+    export default {
+      name: "MainTabBar",
+      components: {
+        TabBar,
+        TabBarItem
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
